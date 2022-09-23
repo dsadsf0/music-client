22c438932dd4905fd4c27167b3105d19c1f1939e
dsadsf0 <p.saraew@yandex.ru>
2022-09-23 14:32:04 +0300

    update

--- src/pages/playlist/Playlist.tsx
+++ src/pages/playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, memo, useEffect, useState } from 'react'
+import React, { createRef, memo, useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import PlaylistService from '../../API/PlaylistService';
 import { useFetching } from '../../hooks/fetching';
@@ -11,11 +11,13 @@ import { useAppDispatch, useAppSeletor } from './../../hooks/redux';
 import { playerSlice } from './../../store/reducers/PlayerSlice';
 import { API_URL } from '../../API';
 import getAverageRGB from './../../scrtipts/averageColor';
+import SongBlock from './song_block/SongBlock';
 
 const Playlist = memo(() => {
   const playlisId = useParams().id || ''
   const imgRef = createRef<HTMLImageElement>()
-  const [avgColor, setAvgColor] = useState('rgb(31, 31, 31)')
+  const songListIntroRef = createRef<HTMLDivElement>()
+  const [avgColor, setAvgColor] = useState({r: 31, g:31, b: 31})
   const dispatch = useAppDispatch()
   const { isAuth } = useAppSeletor(state => state.auth)
   const playerSongs = useAppSeletor(state => state.player.songs)
@@ -59,6 +61,36 @@ const Playlist = memo(() => {
       fetchSongs()
   }, [isPlaylistLoading]);
 
+  const coloring = useCallback(() => {
+    const coloring = () => {
+      if (main?.scrollTop && main?.scrollTop >= 310) {
+        if (header) header.style.backgroundColor = `rgb(${avgColor.r * .8},${avgColor.g * .8},${avgColor.b * .8})`
+      }
+      else {
+        if (header) header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+      }
+
+      if (songListIntroRef.current?.getBoundingClientRect().top === 75) {
+        songListIntroRef.current.style.backgroundColor = '#202020'
+      } else {
+        if (songListIntroRef.current) songListIntroRef.current.style.backgroundColor = 'transparent'
+      }
+    }
+
+    const header = document.querySelector('header')
+    const main = document.querySelector('main')
+    main?.addEventListener('scroll', coloring)
+
+    return () => {
+      main?.removeEventListener('scroll', coloring)
+      header ? header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)' : console.log();
+    }
+  }, [songListIntroRef])
+
+  useEffect((()=> {
+    coloring()
+  }), [songListIntroRef])
+
   useEffect(() => {
     if (isAuth && !isSongsLoading && !playerSongs.length) dispatch(playerSlice.actions.setSongs(songs))
     if (autoplay && isAuth && !isPlaylistLoading) 
@@ -83,70 +115,82 @@ const Playlist = memo(() => {
   return (
     <div 
       className={cl.playlist}
-      style={{ backgroundImage: `linear-gradient(${avgColor} 0px, rgb(14, 14, 14) 600px)` }}
+      
     >
-      <div className={cl.playlist__header}>
-        <div className={cl.cover}>
-          <img 
-            src={`${API_URL}/covers/${playlist?.cover || 'nf.png'}`} 
-            alt={`playlist cover: ${playlist.title}`}
-            ref={imgRef}
-            onLoad={() => {
-              if (imgRef?.current) {
-                const color = getAverageRGB(imgRef?.current)
-                setAvgColor(`rgb(${color.r},${color.g},${color.b})`)
-              }
-            }}
-          />
-        </div>
-        <div className={cl.info}>
-          <span className={cl.upperTitle}>playlist</span>
-          <h1 className={cl.title}>{playlist.title}</h1>
-          <p className={cl.description}>{playlist.description}</p>
-        </div>
-      </div>
-      <button
-        className={cl.btn}
-        onClick={setPlayer}
+      <div className={cl.playlist__header}
+        style={{ backgroundImage: `linear-gradient(rgb(${avgColor.r * 1.2},${avgColor.g * 1.2},${avgColor.b*1.2}) 0%, rgb(${avgColor.r * 0.65},${avgColor.g * 0.65},${avgColor.b * 0.65}) 100%)` }}
       >
-        {
-          isPause || playerSongs[0]?._id !== songs[0]?._id
-          ?
-            <svg 
-            className={cl.play}
-              x="0px" y="0px" viewBox="0 0 494.148 494.148"
-            >
-              <path d="M405.284,201.188L130.804,13.28C118.128,4.596,105.356,0,94.74,0C74.216,0,61.52,16.472,61.52,44.044v406.124c0,27.54,12.68,43.98,33.156,43.98c10.632,0,23.2-4.6,35.904-13.308l274.608-187.904c17.66-12.104,27.44-28.392,27.44-45.884C432.632,229.572,422.964,213.288,405.284,201.188z" />
-            </svg>
-          : 
-            <svg 
-              className={cl.pause} 
-              x="0px" y="0px" viewBox="0 0 47.607 47.607">
-              <path d="M17.991,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631C4.729,2.969,7.698,0,11.36,0l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z" fill="currentColor" />
-              <path d="M42.877,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631C29.616,2.969,32.585,0,36.246,0l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z" fill="currentColor" />
-            </svg>
-        }
-      </button>
+        <div className={cl.playlist__intro}>
+          <div className={cl.cover}>
+            <img
+              src={`${API_URL}/covers/${playlist?.cover || 'nf.png'}`}
+              alt={`playlist cover: ${playlist.title}`}
+              ref={imgRef}
+              onLoad={() => {
+                if (imgRef?.current) {
+                  const color = getAverageRGB(imgRef?.current)
+                  setAvgColor({ r: color.r, g: color.g, b: color.b })
+                }
+              }}
+            />
+          </div>
+          <div className={cl.info}>
+            <span className={cl.upperTitle}>playlist</span>
+            <h1 className={cl.title}>{playlist.title}</h1>
+            <p className={cl.description}>{playlist.description}</p>
+          </div>
+        </div>
+        <button
+          className={cl.btn}
+          onClick={setPlayer}
+        >
+          {
+            isPause || playerSongs[0]?._id !== songs[0]?._id
+              ?
+              <svg
+                className={cl.play}
+                x="0px" y="0px" viewBox="0 0 494.148 494.148"
+              >
+                <path d="M405.284,201.188L130.804,13.28C118.128,4.596,105.356,0,94.74,0C74.216,0,61.52,16.472,61.52,44.044v406.124c0,27.54,12.68,43.98,33.156,43.98c10.632,0,23.2-4.6,35.904-13.308l274.608-187.904c17.66-12.104,27.44-28.392,27.44-45.884C432.632,229.572,422.964,213.288,405.284,201.188z" />
+              </svg>
+              :
+              <svg
+                className={cl.pause}
+                x="0px" y="0px" viewBox="0 0 47.607 47.607">
+                <path d="M17.991,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631C4.729,2.969,7.698,0,11.36,0l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z" fill="currentColor" />
+                <path d="M42.877,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631C29.616,2.969,32.585,0,36.246,0l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z" fill="currentColor" />
+              </svg>
+          }
+        </button>
+      </div>
       <div className={cl.songList}>
-        <div className={`${cl.songList__songBlock} ${cl.songList__block_intro}`}>
-
+        <div 
+          className={cl.songList__intro}
+          ref={songListIntroRef}
+        >
+          <div className={cl.songBlock__index}>
+            #
+          </div>
+          <div className={cl.songBlock__title}>
+            title
+          </div>
         </div>
         {
-          songs.map((song) =>
-            <div
-              className={cl.songList__songBlock}
+          songs.map((song, i) =>
+            <SongBlock
+              className={cl.songBlock}
               key={song._id}
-            >
-              {song.name}
-            </div>
+              song={song}
+              index={i}
+              playlistCover={playlist.cover}
+            />
           )
         }
         {
           isSongsLoading ? <Loader /> : null
         }
+        {/* <div className={cl.test}></div> */}
       </div>
-      
-      <div className={cl.test}></div>
     </div>
   )
 })
--- src/pages/playlist/song_block/SongBlock.tsx
+++ src/pages/playlist/song_block/SongBlock.tsx
@@ -1,18 +1,31 @@
 import React, { memo } from 'react'
-import cl from './sonBlock.module.scss'
+import cl from './songBlock.module.scss'
 import classNameCheck from '../../../scrtipts/classNameCheck'
 import BaseProps from '../../../types/BaseProps'
 import ISong from './../../../types/ISong';
+import { API_URL } from '../../../API';
 
 interface Props extends BaseProps {
   song: ISong,
   index?: number,
+  playlistCover: string
 }
 
-const SongBlock = memo(({className, index, song}: Props) => {
+const SongBlock = memo(({className, index, song, playlistCover}: Props) => {
   return (
     <div className={`${cl.container} ${classNameCheck(className)}`}>
-      
+      <div className={cl.index}>{index}</div>
+      <div className={cl.info}>
+        <div 
+          className={cl.info__cover}
+          style={{ backgroundImage: `url(${API_URL}/covers/${song?.cover || playlistCover})` }}
+        >
+        </div>
+        <div className={cl.info__text}>
+          <div className={cl.info__name}>{song.name}</div>
+          <div className={cl.info__author}>{song.author}</div>
+        </div>
+      </div>
     </div>
   )
 })
