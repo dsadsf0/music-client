ee56a54925d4edaed553cb40ff7f25755d9c4eaf
dsadsf0 <p.saraew@yandex.ru>
2022-10-12 12:24:54 +0300

    added liked songs page

--- src/API/UserService.ts
+++ src/API/UserService.ts
@@ -1,12 +1,37 @@
 import api, { API_URL } from ".";
 import IUser from "../types/IUser";
+import ISong from './../types/ISong';
 
 export default class UserService {
+  
   static async likeSong(userId: string, songId: string) {
-    return await api.put<IUser>(`${API_URL}/api/user/like/song`, { userId, songId })
+    try {
+      const res = await api.put<IUser>(`${API_URL}/api/user/like/song`, { userId, songId })
+      return res.data
+    }
+    catch (error) {
+      throw error
+    }
   }
 
   static async likePlaylist(userId: string, playlistId: string) {
-    return await api.put<IUser>(`${API_URL}/api/user/like/playlist`, { userId, playlistId })
+    try {
+      const res = await api.put<IUser>(`${API_URL}/api/user/like/playlist`, { userId, playlistId })
+      return res.data
+    }
+    catch (error) {
+      throw error
+    }
   }
+
+  static async getLikedSongs(userId: string) {
+    try {
+      const res = await api.get<ISong[]>(`${API_URL}/api/user/like/song/${userId}`)
+      return res.data
+    }
+    catch (error) {
+      throw error
+    }
+  }
+  
 }
