3ebc0b160054821572bf293475aba3377b832ce6
dsadsf0 <p.saraew@yandex.ru>
2022-11-02 12:16:36 +0300

    updates, fixes

--- src/pages/genres/Genres.tsx
+++ src/pages/genres/Genres.tsx
@@ -1,10 +1,84 @@
-import React, { memo } from 'react'
+import React, { createRef, memo, useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import PlaylistService from '../../API/PlaylistService'
+import Loader from '../../components/UI/loader/Loader'
+import { useFetching } from '../../hooks/fetching'
+import { useAppSeletor } from '../../hooks/redux'
+import mainRoutes from '../../routes/mainRoutes'
+import IPlaylist from '../../types/IPlaylist'
+import PlaylistBlock from '../home/playlistBlock/playlistBlock'
 import cl from './genres.module.scss'
 
+let observer: MutationObserver
+
 const Genres = memo(() => {
+  
+  const { isAuth } = useAppSeletor(state => state.auth)
+  const [playlists, setPlaylists] = useState<IPlaylist[]>([])
+  const playlistSectionRef = createRef<HTMLDivElement>()
+
+  const [fetchPlaylists, isPlaylistsLoading, fetchPlaylistsError] = useFetching(async () => {
+    const fetchedPlaylists = await PlaylistService.getAllPlaylists()
+    setPlaylists(fetchedPlaylists)
+    observer = new MutationObserver(mutationRecords => {
+      const section = mutationRecords[mutationRecords.length - 1].target as HTMLElement
+      let isA = true;
+      section.childNodes.forEach(item => {
+        if (item.nodeName.toLowerCase() !== 'a') isA = false
+      })
+
+      if (isA) {
+        const sectionWidth = section.clientWidth
+        const a = section.childNodes[0] as HTMLElement
+        const countInRow = Math.floor((sectionWidth + 30) / (a.clientWidth + 30))
+        
+        if (countInRow < section.childNodes.length) {
+          section.childNodes.forEach(item => {
+            const link = item as HTMLElement
+            const additionalWidth = Math.floor((sectionWidth - ((a.clientWidth + 30) * countInRow) + 30) / countInRow)
+            link.style.height = Math.ceil(a.clientHeight) + additionalWidth + 'px'
+            link.style.width = Math.ceil(a.clientWidth) + additionalWidth + 'px'
+
+          })
+        }
+      }
+    });
+  })
+
+  useEffect(() => {
+    fetchPlaylists()
+  }, [])
+
+  useEffect(() => {
+    if (playlistSectionRef.current && playlists.length) {
+      observer.observe(playlistSectionRef.current, { childList: true })
+    }
+
+    return () => {
+      if (observer) observer.disconnect()
+    }
+  }, [isPlaylistsLoading])
+
+  if (!isAuth)
+    return <Navigate to={mainRoutes.login} replace={true} />
+
+  if (isPlaylistsLoading)
+    return <Loader/>
+  
   return (
     <div className={cl.container}>
-      <h2 className={cl.title}>Genres</h2>
+      <h2 className={cl.title}>Browse all playlists</h2>
+      <section className={cl.content} ref={playlistSectionRef}>
+        {
+          playlists.map((playlist) =>
+            <PlaylistBlock
+              key={playlist._id}
+              className={cl.playlistBlock}
+              playlistId={playlist._id}
+            />
+          )
+        }
+      </section>
     </div>
   )
 })
